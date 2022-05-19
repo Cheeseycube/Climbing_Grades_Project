3e8f547adab9fb077883068b5301d234d5644c9a
Joseph Rousoss <jroussos01@gmail.com>
2022-05-19 00:47:19 -0400

    added some distribution charts

--- Climbing_Grade_Proj_Data_Exploration.py
+++ Climbing_Grade_Proj_Data_Exploration.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import RFE
-
+import seaborn as sns
 
 mydata = pd.read_excel("Climbing_Stats.xlsx")
 
@@ -20,7 +20,7 @@ mydata = mydata.drop('Distance between holds for intended beta', 1)
 tempdata = mydata
 
 
-# dropping empty rows at the bottom
+# dropping empty rows at the bottom   This works by checking each index in the original dataset, then removing that specific row from the new dataset regardless of index
 for x in range(19, 29):
     tempdata = tempdata.drop([mydata.index[x]])
     
@@ -56,5 +56,48 @@ print("Same thing but using different code:")
 for i in tempdata.index:
     if(tempdata["Given Grade"][i] == 0):
         print(tempdata["Jugs"][i])
+        
 
+# need to make a distribution chart for grades and number of jugs
+
+# distribution including entire dataset
+sns.displot(tempdata, x="Jugs")
+
+# creating a dataset with only V0s
+V0data = tempdata
+for i in tempdata.index:
+    if (tempdata["Given Grade"][i] != 0):
+        V0data = V0data.drop([tempdata.index[i]])
+
+# distribution of jugs for V0s only        
+sns.displot(V0data, x="Jugs")
+
+
+# creating a dataset with only V1s
+V1data = tempdata
+for i in tempdata.index:
+    if (tempdata["Given Grade"][i] != 1):
+        V1data = V1data.drop([tempdata.index[i]])
+
+# distribution of jugs for V1s only        
+sns.displot(V1data, x="Jugs")
+
+
+# creating a dataset with only V2s
+V2data = tempdata
+for i in tempdata.index:
+    if (tempdata["Given Grade"][i] != 2):
+        V2data = V2data.drop([tempdata.index[i]])
+
+# distribution of jugs for V2s only        
+sns.displot(V2data, x="Jugs")
+
+# creating a dataset with only V3s
+V3data = tempdata
+for i in tempdata.index:
+    if (tempdata["Given Grade"][i] != 3):
+        V3data = V3data.drop([tempdata.index[i]])
+
+# distribution of jugs for V3s only        
+sns.displot(V3data, x="Jugs")
 
