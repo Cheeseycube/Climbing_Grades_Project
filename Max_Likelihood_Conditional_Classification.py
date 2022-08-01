629fdd582ed536a5a2c4af113c60cddda13cb20a
Joseph Rousoss <jroussos01@gmail.com>
2022-08-01 02:50:24 -0400

    added a new file

---
+++ Max_Likelihood_Conditional_Classification.py
@@ -0,0 +1,6 @@
+# Climbing Grade Project 
+# Max Likliehood Conditional Classification
+
+# prob_num_Jugs_GivenV0 * prob_numCrimps_GivenV0 * prob_numFootholds_GivenV0 = probData_GivenV0
+# check independence with chi-squared test of independence, the null is independent
+# do this by making the two way table and putting that in a chi-squared calculator
